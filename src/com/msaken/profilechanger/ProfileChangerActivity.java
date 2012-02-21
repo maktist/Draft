0e6258360d195056c333f458b529539a204ded6b
alakhal <blaksos@hotmail.com>
2012-02-21 16:18:28 +0100

    add project skeleton

---
+++ src/com/msaken/profilechanger/ProfileChangerActivity.java
@@ -0,0 +1,13 @@
+package com.msaken.profilechanger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileChangerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
